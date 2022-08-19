3ad844f2f63831b94bc5f6a48188a1eac3efd71c
Irfan Ansari <96610209+Irfu120@users.noreply.github.com>
2022-08-19 11:54:00 +0530

    Add files via upload

---
+++ Language detection.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Irfan
+"""
+        #Language Detection
+#pip install langdetect
+from langdetect import detect
+text = input("Enter any text in any language")
+print(detect(text))
+
+
+
+
+
+
+
+
